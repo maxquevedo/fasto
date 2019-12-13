6df91273fe0376e15d3a70ae32d6b27ec2d865dc
Maximiliano Quevedo <maxquevedo@gmail.com>
2019-12-12 23:13:34 -0300

    database 1.6

--- db/v1.6.sql
+++ db/v1.6.sql
@@ -2,7 +2,7 @@ CREATE TABLE "agenda_usuario" (
   "id" int4 NOT NULL,
   "id_servicio" int4 NOT NULL,
   "id_cliente_agenda" int4,
-  "id_venta" int4,
+  "id_venta" int4 NOT NULL,
   "fecha_reservada" date,
   PRIMARY KEY ("id")
 );
@@ -51,7 +51,7 @@ CREATE TABLE "pago" (
 
 CREATE TABLE "perfil" (
   "id" int4 NOT NULL,
-  "descripcion" varchar NOT NULL,
+  "descripcion" varchar(255) NOT NULL,
   "grado" int4 NOT NULL,
   PRIMARY KEY ("id")
 );
@@ -78,7 +78,7 @@ CREATE TABLE "servicio" (
 CREATE TABLE "sistema" (
   "id" int4 NOT NULL,
   "id_usuario" int4,
-  "id_sistema_especifico" varchar(255),
+  "id_sistema_especifico" int4,
   "fecha_inicio" timestamp NOT NULL,
   "fecha_fin" timestamp NOT NULL,
   PRIMARY KEY ("id")
@@ -88,7 +88,7 @@ CREATE TABLE "sistema_agenda" (
   "id" int4 NOT NULL,
   "id_tematica" int4 NOT NULL,
   "id_agenda_usuario" int4 NOT NULL,
-  "nombre" varchar NOT NULL,
+  "nombre" varchar(255) NOT NULL,
   PRIMARY KEY ("id")
 );
 
@@ -138,7 +138,6 @@ CREATE TABLE "venta_agenda" (
   "id" int4 NOT NULL,
   "id_pago" int4,
   "id_cliente_agenda" int4,
-  "id_servicio" int4,
   "abono" int4,
   PRIMARY KEY ("id")
 );
@@ -152,22 +151,23 @@ CREATE TABLE "venta_eshop" (
   PRIMARY KEY ("id")
 );
 
-
-
-
-
-
-
-
-
-ALTER TABLE "sistema_especifico" ADD CONSTRAINT "fk_sistema_especifico_sistema_eshop_1" FOREIGN KEY ("id") REFERENCES "sistema_eshop" ("id");
-ALTER TABLE "sistema_especifico" ADD CONSTRAINT "fk_sistema_especifico_sistema_1" FOREIGN KEY ("id") REFERENCES "sistema" ("id_sistema_especifico");
+ALTER TABLE "agenda_usuario" ADD CONSTRAINT "fk_agenda_usuario_servicio_1" FOREIGN KEY ("id_servicio") REFERENCES "servicio" ("id");
+ALTER TABLE "agenda_usuario" ADD CONSTRAINT "fk_agenda_usuario_clientes_agenda_1" FOREIGN KEY ("id_cliente_agenda") REFERENCES "clientes_agenda" ("id");
+ALTER TABLE "inventario" ADD CONSTRAINT "fk_tienda_invent" FOREIGN KEY ("id_eshop") REFERENCES "sistema_eshop" ("id") ON DELETE CASCADE;
+ALTER TABLE "inventario" ADD CONSTRAINT "fk_producto_invent" FOREIGN KEY ("id_producto") REFERENCES "producto" ("id") ON DELETE CASCADE;
+ALTER TABLE "producto" ADD CONSTRAINT "fk_categoria_prod" FOREIGN KEY ("id_categoria") REFERENCES "categoria" ("id") ON DELETE CASCADE;
+ALTER TABLE "sistema_agenda" ADD CONSTRAINT "fk_tematica_agenda" FOREIGN KEY ("id_tematica") REFERENCES "tematica" ("id") ON DELETE CASCADE;
+ALTER TABLE "sistema_agenda" ADD CONSTRAINT "fk_agenda_sistema" FOREIGN KEY ("id_agenda_usuario") REFERENCES "agenda_usuario" ("id") ON DELETE CASCADE;
+ALTER TABLE "sistema_eshop" ADD CONSTRAINT "fk_tematica_eshop" FOREIGN KEY ("id_tematica") REFERENCES "tematica" ("id") ON DELETE CASCADE;
+ALTER TABLE "sistema_especifico" ADD CONSTRAINT "fk_sistema_especifico_sistema_especifico_1" FOREIGN KEY ("id_sistema") REFERENCES "sistema" ("id") ON DELETE CASCADE;
+ALTER TABLE "sistema_especifico" ADD CONSTRAINT "fk_sistema_especifico_sistema_eshop_1" FOREIGN KEY ("id_especifico") REFERENCES "sistema_eshop" ("id") ON DELETE CASCADE;
+ALTER TABLE "sistema_especifico" ADD CONSTRAINT "fk_sistema_especifico_agenda" FOREIGN KEY ("id_especifico") REFERENCES "sistema_agenda" ("id") ON DELETE CASCADE;
 ALTER TABLE "usuario" ADD CONSTRAINT "fk_perfil_usuario" FOREIGN KEY ("id_perfil") REFERENCES "perfil" ("id") ON DELETE CASCADE;
 ALTER TABLE "usuario_sistema" ADD CONSTRAINT "fk_usuario_sistema" FOREIGN KEY ("id_usuario") REFERENCES "usuario" ("id") ON DELETE CASCADE;
 ALTER TABLE "usuario_sistema" ADD CONSTRAINT "fk_sistema_usuario" FOREIGN KEY ("id_sistema") REFERENCES "sistema" ("id") ON DELETE CASCADE;
-ALTER TABLE "venta_agenda" ADD CONSTRAINT "fk_venta_agenda_agenda_usuario_1" FOREIGN KEY ("id") REFERENCES "agenda_usuario" ("id_venta");
-ALTER TABLE "venta_agenda" ADD CONSTRAINT "fk_venta_agenda_pago_1" FOREIGN KEY ("id_pago") REFERENCES "pago" ("id");
-ALTER TABLE "venta_eshop" ADD CONSTRAINT "fk_cliente_venta" FOREIGN KEY ("id_cliente_eshop") REFERENCES "cliente_eshop" ("id") ON DELETE CASCADE;
-ALTER TABLE "venta_eshop" ADD CONSTRAINT "fk_producto_venta" FOREIGN KEY ("id_producto") REFERENCES "producto" ("id") ON DELETE CASCADE;
-ALTER TABLE "venta_eshop" ADD CONSTRAINT "fk_pago_venta" FOREIGN KEY ("id_pago") REFERENCES "pago" ("id") ON DELETE CASCADE;
+
+
+
+
+
 
